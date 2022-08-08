2248e5d23602cc5f28b7236baf99d1605f5232f3
Priyanka Gupta <47624791+priyanka1121@users.noreply.github.com>
2022-08-08 19:30:56 +0530

    Create 1. TwoSum.py

---
+++ 1. TwoSum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        Map = {} #mapping the value to the index of that value. Previous value is going to be mapped
+        for i, n in enumerate(nums):   
+            dif = target - n 
+            if dif in Map:  #if the difference is already in the map return the solution 
+                return [Map[dif],i]
+            Map[n] = i #if dont continue it
+        return
+        
+#complexity of O(n)
