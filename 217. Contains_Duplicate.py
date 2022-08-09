ffbc059f7bd68e21e5c2250faad0d7cef25cb1b0
Priyanka Gupta <47624791+priyanka1121@users.noreply.github.com>
2022-08-09 19:21:26 +0530

    Added approach 3 solution

--- 217. Contains_Duplicate.py
+++ 217. Contains_Duplicate.py
@@ -2,3 +2,15 @@
 # Approach 1: Brute force solution. Time: O(n^2) Space : O(1)
 # Approach 2: Sorting the array. Time: O(nlogn) Space: O(1)
 # Approach 3: HashSet Time: O(n) Space: O(n)
+# Solution using HashSet
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        Set = set()
+        
+        for n in nums:
+            if n in Set:
+                return True
+            Set.add(n)
+        return False
+            
