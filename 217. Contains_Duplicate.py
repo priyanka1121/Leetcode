f0a5eeea1c40b1f8aa0bfc3197ea5e385bb9f6ac
Priyanka Gupta <47624791+priyanka1121@users.noreply.github.com>
2022-08-09 19:15:36 +0530

    Create 217. Contains_Duplicate.py

---
+++ 217. Contains_Duplicate.py
@@ -0,0 +1,4 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+# Approach 1: Brute force solution. Time: O(n^2) Space : O(1)
+# Approach 2: Sorting the array. Time: O(nlogn) Space: O(1)
+# Approach 3: HashSet Time: O(n) Space: O(n)
