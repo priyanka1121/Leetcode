40a25b5fdf4ccd8e68face8bdaed931f9092c733
Priyanka Gupta <47624791+priyanka1121@users.noreply.github.com>
2022-08-09 19:57:42 +0530

    Create 242. Valid_Anagram.py

---
+++ 242. Valid_Anagram.py
@@ -0,0 +1,23 @@
+ #Approach 1: Use Hashmap 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #return Counter(s) == Counter(t)
+       
+        
+        #if length of strings are not equal
+        
+        if len(s) != len(t):
+            return False
+        
+        #Define Hash maps for both of strings
+        countS, countT = {}, {}
+        
+        #creating the hash maps
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i],0)
+            countT[t[i]] = 1 + countT.get(t[i],0)
+        #if count is not equal
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+        return True
