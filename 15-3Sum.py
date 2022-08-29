d5d9606e0b5eba19c482063c164903deb677ad8e
Priyanka Gupta <47624791+priyanka1121@users.noreply.github.com>
2022-08-29 19:53:59 +0530

    Create 15-3Sum.py

---
+++ 15-3Sum.py
@@ -0,0 +1,26 @@
+# Time: O(n log n) + O(n^2) = O(n^2)
+# Space: O(1) or O(n)
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):  #for first index and to check if the next number is also same or not
+            if i > 0 and a == nums[i - 1]:
+                continue
+
+            l, r = i + 1, len(nums) - 1  #same 2sum logic
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return res
