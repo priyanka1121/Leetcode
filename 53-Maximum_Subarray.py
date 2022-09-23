63ea68b6f006161cae90e9b95200e2abef08347d
Priyanka Gupta <47624791+priyanka1121@users.noreply.github.com>
2022-09-23 20:15:27 +0530

    Create 53-Maximum_Subarray.py

---
+++ 53-Maximum_Subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curSum =  0   
+        maxSum = nums[0]
+        for n in nums:
+            if curSum < 0:  #to remove negative prefix , if we encounter with a negative number then we will make the sum to 0
+                curSum = 0
+            curSum += n
+            if curSum > maxSum:  #updating
+                maxSum = curSum
+        return maxSum
+# Time: O(n)
